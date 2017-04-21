d0ded1139fc6f1d494d23015cb1b35b6ce01dbe0
AshishBora <ashish.dilip.bora@gmail.com>
2017-04-21 04:17:12 -0500

    deleting unnecessary files

--- transport.sh
+++
@@ -1,5 +0,0 @@
-REMOTE1=abora@microdeep.ece.utexas.edu:/home/abora/mnist-vae/samples
-REMOTE2=abora@microdeep.ece.utexas.edu:/home/abora/mnist-vae/models
-LOCAL=./
-rsync -aurz -e 'ssh -p 52617' --progress $REMOTE1 $LOCAL
-rsync -aurz -e 'ssh -p 52617' --progress $REMOTE2 $LOCAL
